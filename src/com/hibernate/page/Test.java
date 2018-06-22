c750e41a4410d266e1dce9d4a163fce5271efdb0
Kathir1988 <kathirmecse@gmail.com>
2018-06-22 17:17:31 +0530

    Modified class

--- src/com/hibernate/page/Test.java
+++ src/com/hibernate/page/Test.java
@@ -4,12 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 	
-		//CRUDOper c = new CRUDOper();
-		Data d=new Data();
-		//c.insert(d);
-		//c.load(d);
-		//c.update(d);
-		//c.delete(d);
+		System.out.println("Welcome");
 	}
 
 }
