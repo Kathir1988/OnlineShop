ce966b3c414504b807fe2a38fddf70d37846ae5f
Kathir1988 <kathirmecse@gmail.com>
2018-06-22 17:06:00 +0530

    Added class file

---
+++ src/com/hibernate/page/Sample.java
@@ -0,0 +1,21 @@
+package com.hibernate.page;
+
+public class Sample {
+	
+	public int age;
+	public String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
---
+++ src/com/hibernate/page/Test.java
@@ -0,0 +1,15 @@
+package com.hibernate.page;
+
+public class Test {
+	
+	public static void main(String[] args) {
+	
+		//CRUDOper c = new CRUDOper();
+		Data d=new Data();
+		//c.insert(d);
+		//c.load(d);
+		//c.update(d);
+		//c.delete(d);
+	}
+
+}
